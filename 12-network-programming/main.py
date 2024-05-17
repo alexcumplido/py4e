46db0766ac5e4bd6f81efeee67db6ba7643e9877
acumplid <alexandre.cumplido@bsc.es>
2024-05-17 15:17:45 +0200

    network programming

---
+++ 12-network-programming/main.py
@@ -0,0 +1,99 @@
+import socket
+import time
+import urllib.request, urllib.parse, urllib.error
+import re
+import ssl
+
+# # Retrieve data over HTTP
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data)
+mysock.close()
+
+# # Retrieve an image over HTTP
+HOST = 'data.pr4e.org'
+PORT = 80
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((HOST, PORT))
+mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\r\n\r\n')
+count = 0
+picture = b""
+
+while True:
+    data = mysock.recv(5120)
+    if len(data) < 1: break
+    # time.sleep(0.25)
+    count = count + len(data)
+    print(len(data), count)
+    picture = picture + data
+mysock.close()
+
+# # Look for the end of the header (2 CRLF)
+pos = picture.find(b"\r\n\r\n")
+print('Header length', pos)
+print(picture[:pos].decode())
+
+# # Skip past the header and save the picture data
+picture = picture[pos+4:]
+fhand = open("stuff.jpg", "wb")
+fhand.write(picture)
+fhand.close()
+
+# Retrieve data over HTTP using urllib
+
+count = dict()
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand: 
+    line = line.decode().strip()
+    for w in line.split():
+        count[w] = count.get(w, 0) + 1
+print(count)
+
+img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg')
+fhand = open('cover3.jpg', 'wb')
+size = 0
+while True:
+    info = img.read(100000)
+    if len(info) < 1: break
+    size = size + len(info)
+    fhand.write(info)
+
+print(size, 'characters copied.')
+fhand.close()
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+links = re.findall(b'href="(http[s]?://.*?)"', html)
+for link in links: print(link.decode())
+
+# Parsing HTML using BeautifulSoup
+
+from bs4 import BeautifulSoup
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup =  BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags: 
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents)
+    print('Attrs:', tag.attrs)
